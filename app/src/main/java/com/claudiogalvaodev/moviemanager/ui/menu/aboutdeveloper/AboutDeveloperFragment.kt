e55214c7d8da281af5cc7fb26a0ac3cad23f0970
claudiogaalvao <claudiogaalvao@gmail.com>
2022-02-11 19:09:42 -0300

    Redirect to social media

--- app/src/main/java/com/claudiogalvaodev/moviemanager/ui/menu/aboutdeveloper/AboutDeveloperFragment.kt
+++ app/src/main/java/com/claudiogalvaodev/moviemanager/ui/menu/aboutdeveloper/AboutDeveloperFragment.kt
@@ -1,5 +1,7 @@
 package com.claudiogalvaodev.moviemanager.ui.menu.aboutdeveloper
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +21,20 @@ class AboutDeveloperFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fragmentAboutDeveloperInstagramSocialMediaCard.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/claudiogaalvao/?hl=pt-br"))
+            startActivity(browserIntent)
+        }
+
+        binding.fragmentAboutDeveloperLinkedinSocialMediaCard.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/claudiogaalvao/"))
+            startActivity(browserIntent)
+        }
     }
 
 }
