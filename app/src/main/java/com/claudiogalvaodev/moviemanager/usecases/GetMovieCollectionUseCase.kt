66113f9317bb84d512141fe161d7ac030316996f
claudiogaalvao <claudiogaalvao@gmail.com>
2022-02-11 21:05:35 -0300

    Show collection movies in the correct sequence

--- app/src/main/java/com/claudiogalvaodev/moviemanager/usecases/GetMovieCollectionUseCase.kt
+++ app/src/main/java/com/claudiogalvaodev/moviemanager/usecases/GetMovieCollectionUseCase.kt
@@ -1,14 +1,38 @@
 package com.claudiogalvaodev.moviemanager.usecases
 
 import com.claudiogalvaodev.moviemanager.data.model.Collection
+import com.claudiogalvaodev.moviemanager.data.model.Movie
 import com.claudiogalvaodev.moviemanager.data.repository.MoviesRepository
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class GetMovieCollectionUseCase(
     private val repository: MoviesRepository
 ) {
 
     suspend operator fun invoke(movieId: Int): Result<Collection?> {
-        return repository.getCollection(movieId)
+        val collectionResult = repository.getCollection(movieId)
+        if(collectionResult.isSuccess) {
+            val collection = collectionResult.getOrNull()
+            if(collection != null) {
+                val sortedCollection = Collection(
+                    id = collection.id,
+                    name = collection.name,
+                    poster_path = collection.poster_path,
+                    backdrop_path = collection.backdrop_path,
+                    parts = sortCollection(collection.parts)
+                )
+                return Result.success(sortedCollection)
+            }
+        }
+        return collectionResult
+    }
+
+    private fun sortCollection(movies: List<Movie>): List<Movie> {
+        val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        return movies.sortedByDescending { movie ->
+            LocalDate.parse(movie.release_date, dateTimeFormatter)
+        }.asReversed()
     }
 
 }
