a33d9590c0da1118af31edf44ad880aa63f503f4
Claudio Galvão <claudiogaalvao@gmail.com>
2024-05-20 21:01:09 -0300

    Create FilterBaseScreen

--- app/src/main/java/com/claudiogalvaodev/moviemanager/ui/filter/screens/FilterProviderScreen.kt
+++ app/src/main/java/com/claudiogalvaodev/moviemanager/ui/filter/screens/FilterProviderScreen.kt
@@ -1,27 +1,17 @@
 package com.claudiogalvaodev.moviemanager.ui.filter.screens
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Checkbox
-import androidx.compose.material.CheckboxDefaults
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.claudiogalvaodev.moviemanager.ui.filter.FiltersViewModel
-import com.claudiogalvaodev.moviemanager.ui.menu.schedulednotifications.ui.theme.LightPurple
+import com.claudiogalvaodev.moviemanager.ui.filter.components.CheckboxItem
+import com.claudiogalvaodev.moviemanager.ui.filter.components.FilterBaseScreen
 import com.claudiogalvaodev.moviemanager.ui.model.ProviderModel
 import org.koin.androidx.compose.koinViewModel
 
@@ -35,12 +25,18 @@ internal fun FilterProviderScreen(
         viewModel.getProviders()
     }
 
-    FilterList(
-        providers = providers,
-        onSelectProvider = { providerId ->
-            viewModel.selectProvider(providerId)
+    FilterBaseScreen(
+        onApplyFilter = {
+            // TODO: Implement apply filter
         }
-    )
+    ) {
+        FilterList(
+            providers = providers,
+            onSelectProvider = { providerId ->
+                viewModel.selectProvider(providerId)
+            }
+        )
+    }
 }
 
 @Composable
@@ -61,50 +57,6 @@ private fun FilterList(
             )
         }
     }
-}
-
-@Composable
-fun CheckboxItem(
-    text: String,
-    checked: Boolean,
-    onCheckedChange: (Boolean) -> Unit
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 12.dp)
-            .border(
-                1.dp,
-                if (checked) LightPurple else Color.Transparent,
-                RoundedCornerShape(8.dp)
-            )
-            .padding(start = 12.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = text,
-            color = Color.White
-        )
-        Checkbox(
-            checked = checked,
-            onCheckedChange = onCheckedChange,
-            colors = CheckboxDefaults.colors(
-                uncheckedColor = Color.LightGray,
-                checkedColor = LightPurple
-            )
-        )
-    }
-}
-
-@Composable
-@Preview(backgroundColor = 0xFF070D2D, showBackground = true)
-private fun CheckboxItemPreview() {
-    CheckboxItem(
-        text = "Netflix",
-        checked = true,
-        onCheckedChange = {}
-    )
 }
 
 @Composable
